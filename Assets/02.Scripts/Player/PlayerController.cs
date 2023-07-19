eb12c3b7807c5ec9567ace5165c1e1a70b1e7183
crazykim67 <crazykim67@gmail.com>
2023-07-20 06:31:07 +0900

    PlayerController Add

--- Assets/02.Scripts/Player/PlayerController.cs
+++ Assets/02.Scripts/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 using UnityEngine.Windows;
 
 public class PlayerController : MonoBehaviour
@@ -17,27 +18,35 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float gravityValue = -10f;
 
+    private InputManager input;
     private Transform cameraTransform;
+
+    [SerializeField]
+    private Transform groundRayTr;
+    [SerializeField]
+    private float rayDistance;
 
     private void Start()
     {
         ch = GetComponent<CharacterController>();
+        input = InputManager.Instance;
         cameraTransform = Camera.main.transform;
 
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        groundedPlayer = ch.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
+        //groundedPlayer = ch.isGrounded;
+        if (isGround() && playerVelocity.y < 0)
             playerVelocity.y = 0f;
-        }
+
+        Debug.Log(isGround());
+
         Movement();
 
-        if (input.OnJump() && groundedPlayer)
+        if (input.OnJump() && isGround())
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
@@ -48,11 +57,27 @@ public class PlayerController : MonoBehaviour
         OnChangeSpeed();
     }
 
+    public bool isGround()
+    {
+        int layer = (-1) - (1 << LayerMask.NameToLayer("Player"));
+
+        RaycastHit hit;
+        bool isGrounded = false;
+
+        //Debug.DrawRay(groundRayTr.position, Vector3.down * rayDistance, Color.red);
+        if(Physics.Raycast(groundRayTr.position, Vector3.down, out hit, rayDistance, layer))
+        {
+            if (hit.transform != null)
+                isGrounded = true;
+            else
+                isGrounded = false;
+        }
+
+        return isGrounded;
+    }
+
     public void Movement()
     {
-        //if (OptionManager.Instance == null)
-        //    return;
-
         Vector2 movement = input.GetPlayerMovement();
         Vector3 move = new Vector3(movement.x, 0f, movement.y);
 
@@ -65,15 +90,9 @@ public class PlayerController : MonoBehaviour
 
     public void OnChangeSpeed()
     {
-        if (InventoryManager.Instance.isUse)
-        {
-            playerSpeed = 2f;
-            return;
-        }
-
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (UnityEngine.Input.GetKey(KeyCode.LeftShift))
             playerSpeed = 3f;
-        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        else if (UnityEngine.Input.GetKeyUp(KeyCode.LeftShift))
             playerSpeed = 2f;
     }
 }
